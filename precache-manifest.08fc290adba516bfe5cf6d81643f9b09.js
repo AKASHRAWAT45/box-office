1452bdb4e8a5906fa6c23e7107d675fa0e6c4cf9
AKASH <ar0938344@gmail.com>
2021-07-05 02:30:09 +0530

    Updates

---
+++ precache-manifest.08fc290adba516bfe5cf6d81643f9b09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fce2e6679c685ef14da3d22f7133682",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "3705ce6961c777f952ad",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "86c700162d6174a0d56a",
+    "url": "/box-office/static/js/2.af509c17.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/box-office/static/js/2.af509c17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3705ce6961c777f952ad",
+    "url": "/box-office/static/js/main.4773aaf0.chunk.js"
+  },
+  {
+    "revision": "a24731daa79adae5d2cf",
+    "url": "/box-office/static/js/runtime-main.796231bd.js"
+  }
+]);
